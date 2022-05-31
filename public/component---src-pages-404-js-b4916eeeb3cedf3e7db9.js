364f60946d09bcb4e52f8e89cfb44978700661c0
app62 <app62.tech@gmail.com>
2022-05-31 18:39:06 +0530

    chore: add build

---
+++ public/component---src-pages-404-js-b4916eeeb3cedf3e7db9.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkschool_template_this_is_a_best_school=self.webpackChunkschool_template_this_is_a_best_school||[]).push([[883],{429:function(e,s,t){t.r(s);var l=t(7294);s.default=function(){return l.createElement("div",null,"oops !",l.createElement("h2",null,"This is a 404"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b4916eeeb3cedf3e7db9.js.map
